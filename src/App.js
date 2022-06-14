b027d9d257701673dfc1d060517b3173b4ea03ce
Marcos Mora <eduardoavalos399@gmail.com>
2022-06-14 01:35:41 -0500

    feat:add new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState([
+    {
+      name: "John",
+      lastName: "Doe",
+      age: 30,
+      favouriteFood: "Pizza",
+      favouriteColour: "Red",
+      counter: 0,
+    },
+  ]);
+
+  const [name, setName] = useState("");
+  const [lastName, setLastname] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+      type="text"
+      name="name"
+      onChange={(event) => {
+        setName(event.target.value);
+      }}
+      />
+      <input
+      type="text"
+      name="lastName"
+      onChange={(event) => {
+        setLastname(event.target.value);
+      }}
+      />
+      <button
+      onClick={() => {
+        setPeople((current) => [{ name, lastName }, ...current]);
+      }}
+      >
+        Add user
+      </button>
+      <ul>
+      {people.map((person, idx) => (
+        <li id={idx}>{`${person.name} ${person.lastName}`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
